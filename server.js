ce796810c0f4badb5302872b4009d85c77f44f3c
wevdiaz <63312710+wevdiaz@users.noreply.github.com>
2023-05-13 20:26:36 -0300

    create project

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const server = express();
+const nunjucks = require("nunjucks");
+
+server.use(express.static("public"));
+
+server.set("view engine", "html");
+
+nunjucks.configure("./", {
+  express: server,
+  autoescape: true,
+  noCache: true
+});
+
+server.get("/", (req, res) => {
+  res.render("index.html");
+});
+
+server.listen(3000, () => {
+  console.log("http://localhost:3000/");
+  console.log("server is working!");
+});
